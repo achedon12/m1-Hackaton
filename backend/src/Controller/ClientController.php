4b20d8ea5476869fff0712dd055c74db6f424599
Ethan Bourguigneau <ethan.devfs@pm.me>
2025-05-19 16:53:48 +0200

    feat : ajout vérification new client

--- backend/src/Controller/ClientController.php
+++ backend/src/Controller/ClientController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Client;
 use App\Event\ClientCreatedEvent;
+use App\Repository\ClientRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\SecurityBundle\Security;
@@ -23,7 +24,8 @@ final class ClientController extends AbstractController
                                 private readonly UserPasswordHasherInterface $userPasswordHasher,
                                 private readonly EventDispatcherInterface $eventDispatcher,
                                 private readonly TranslatorInterface $translator,
-                                private readonly SluggerInterface $slugger
+                                private readonly SluggerInterface $slugger,
+                                private readonly ClientRepository $clientRepository
     )
     {
     }
@@ -31,24 +33,44 @@ final class ClientController extends AbstractController
     #[Route('/api/client/register', name: 'client_register', methods: ['POST'])]
     public function register(
         Request $request,
-        EntityManagerInterface $em,
-        UserPasswordHasherInterface $passwordHasher
     ): JsonResponse {
         $data = json_decode($request->getContent(), true);
-        if (!isset($data['email']) || !isset($data['password'])) {
-            return new JsonResponse(['error' => 'Missing required fields: email, password'], 400);
+        if (!isset($data['email']) || !isset($data['password']) || !isset($data['phone']) || !isset($data['city']) || !isset($data['zipcode'])) {
+            return new JsonResponse(['error' => 'Champs requis : email, mot de passe, numéro de téléphone, ville, zipcode'], 400);
+        }
+
+        if (!filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
+            return new JsonResponse(['error' => 'Email invalide'], 400);
+        }
+
+        if (strlen($data['password']) < 8) {
+            return new JsonResponse(['error' => 'Le mot de passe doit contenir au moins 8 caractères'], 400);
+        }
+        if (!preg_match('/[A-Z]/', $data['password'])) {
+            return new JsonResponse(['error' => 'Le mot de passe doit contenir au moins une majuscule'], 400);
+        }
+        if (!preg_match('/[0-9]/', $data['password'])) {
+            return new JsonResponse(['error' => 'Le mot de passe doit contenir au moins un chiffre'], 400);
+        }
+        if (!preg_match('/[\W_]/', $data['password'])) {
+            return new JsonResponse(['error' => 'Le mot de passe doit contenir au moins un caractère spéciale'], 400);
+        }
+
+        $existingClient = $this->clientRepository->findOneBy(['email' => $data['email']]);
+        if ($existingClient) {
+            return new JsonResponse(['error' => 'Cette email est déjà utilisé'], 409);
         }
 
         $client = new Client();
         $client->setEmail($data['email']);
         $client->setPassword(
-            $passwordHasher->hashPassword($client, $data['password'])
+            $this->userPasswordHasher->hashPassword($client, $data['password'])
         );
 
         $client->setFirstname($data['firstname'] ?? null);
         $client->setLastname($data['lastname'] ?? null);
-        $client->setPhone($data['phone'] ?? '');
-        $client->setCity($data['city'] ?? '');
+        $client->setPhone($data['phone']);
+        $client->setCity($data['city']);
         $client->setZipcode($data['zipcode'] ?? '');
         $client->setGender($data['gender'] ?? null);
         $client->setSocietyName($data['societyName'] ?? null);
@@ -64,8 +86,8 @@ final class ClientController extends AbstractController
 
         $client->setCreationDate(new \DateTimeImmutable());
 
-        $em->persist($client);
-        $em->flush();
+        $this->entityManager->persist($client);
+        $this->entityManager->flush();
 
         $event = new ClientCreatedEvent($client);
         $this->eventDispatcher->dispatch($event, ClientCreatedEvent::NAME);
