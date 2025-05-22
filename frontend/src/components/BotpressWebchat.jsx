9043f465b3d2bb142b974f42c33754a16b9666f1
Antoine B <aberthillot@myges.fr>
2025-05-22 14:49:20 +0200

    Botpress user data correctly sent

--- frontend/src/components/BotpressWebchat.jsx
+++ frontend/src/components/BotpressWebchat.jsx
@@ -1,95 +1,85 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import config from "../providers/apiConfig.js";
 
 const BotpressChat = () => {
-
     const client = JSON.parse(localStorage.getItem("client"));
+    const [clientVehicles, setClientVehicles] = useState([]);
+    const hasInitializedRef = useRef(false);
 
-    const [clientVehicles, setClientVehicles] = React.useState([]);
+    useEffect(() => {
+        if (!client || hasInitializedRef.current) return;
 
-    const fetchVehicles = async () => {
-        try {
-            const response = await fetch(`${config.apiBaseUrl}/vehicle/client/${client.id}`,
-                {
+        const fetchAndInit = async () => {
+            try {
+                const response = await fetch(`${config.apiBaseUrl}/vehicle/client/${client.id}`, {
                     headers: config.headers,
                 });
 
-            if (!response.ok) {
-                throw new Error(`Erreur ${response.status}`);
-            }
-
-            const data = await response.json();
-            setClientVehicles(data);
-
-        } catch (error) {
-            console.error("Erreur lors de la récupération des véhicules :", error);
-        }
-
-    };
-
-    useEffect(() => {
-        if (!client) {
-            return;
-        }
-        console.log(client);
-        fetchVehicles();
-        console.log(clientVehicles);
-        let clientVehiclesSimplified = clientVehicles.map(vehicleObject => {
-            return {
-                brand: vehicleObject.brand.name,
-                model: vehicleObject.model.name,
-                circulationDate: vehicleObject.circulationDate,
-                mileageKm: vehicleObject.kms,
-                vin: vehicleObject.vin,
-            }
-        })
-        const script = document.createElement("script");
-        script.src = "https://cdn.botpress.cloud/webchat/v2.5/inject.js";
-        script.async = true;
-        script.onload = () => {
-            // Configurer le webchat après le chargement
-            window.botpress.init({
-                "botId": "552aee85-182b-4a41-a55f-3fe1e1eb51d1",
-                "configuration": {
-                    "composerPlaceholder": "Écrivez votre message ici",
-                    "botName": "RD-Vroom IA",
-                    "botAvatar": "https://files.bpcontent.cloud/2025/05/20/13/20250520135832-QND608RS.png",
-                    "botDescription": "Posez-moi vos demandes !",
-                    "website": {},
-                    "email": {},
-                    "phone": {},
-                    "termsOfService": {},
-                    "privacyPolicy": {},
-                    "color": "#cbdeec",
-                    "variant": "solid",
-                    "themeMode": "light",
-                    "fontFamily": "ibm",
-                    "radius": 4,
-                    "storageLocation": "sessionStorage"
-                },
-                "clientId": "d7b487e3-8fff-4fda-9a7e-17a2b6e48bc1",
-                "user": {
-                    "data": {
-                        "firstName": "" + client.firstName,
-                        "lastName": "" + client.lastName,
-                        "gender": "" + client.gender,
-                        "email": "" + client.email,
-                        "phone": "" + client.phone,
-                        "address": "" + client.address,
-                        "city": "" + client.city,
-                        "zipcode": "" + client.zipcode,
-                        "locationLatitude": "" + client.latitude,
-                        "locationLongitude": "" + client.longitude,
-                        "societyName": "" + client.societyName,
-                        "vehiclesJSON" : JSON.stringify(clientVehiclesSimplified)
-                    }
+                if (!response.ok) {
+                    throw new Error(`Erreur ${response.status}`);
                 }
-            });
 
+                const data = await response.json();
+                setClientVehicles(data);
+
+                const clientVehiclesSimplified = data.map(vehicleObject => ({
+                    brand: vehicleObject.brand.name,
+                    model: vehicleObject.model.name,
+                    circulationDate: vehicleObject.circulationDate,
+                    mileageKm: vehicleObject.kms,
+                    vin: vehicleObject.vin,
+                }));
+
+                console.log(client);
+                console.log(clientVehiclesSimplified);
+
+                const script = document.createElement("script");
+                script.src = "https://cdn.botpress.cloud/webchat/v2.5/inject.js";
+                script.async = true;
+                script.onload = () => {
+                    window.botpress.init({
+                        botId: "552aee85-182b-4a41-a55f-3fe1e1eb51d1",
+                        configuration: {
+                            composerPlaceholder: "Écrivez votre message ici",
+                            botName: "RD-Vroom IA",
+                            botAvatar: "https://files.bpcontent.cloud/2025/05/20/13/20250520135832-QND608RS.png",
+                            botDescription: "Posez-moi vos demandes !",
+                            color: "#cbdeec",
+                            variant: "solid",
+                            themeMode: "light",
+                            fontFamily: "ibm",
+                            radius: 4,
+                            storageLocation: "sessionStorage"
+                        },
+                        clientId: "d7b487e3-8fff-4fda-9a7e-17a2b6e48bc1",
+                        user: {
+                            data: {
+                                "firstName": "" + client.firstname,
+                                "lastName": "" + client.lastname,
+                                "gender": "" + client.gender,
+                                "email": "" + client.email,
+                                "phone": "" + client.phone,
+                                "address": "" + client.address,
+                                "city": "" + client.city,
+                                "zipcode": "" + client.zipcode,
+                                "locationLatitude": "" + client.latitude,
+                                "locationLongitude": "" + client.longitude,
+                                "societyName": "" + client.societyName,
+                                "vehiclesJSON" : JSON.stringify(clientVehiclesSimplified)
+                            }
+                        }
+                    });
+                };
+
+                document.body.appendChild(script);
+                hasInitializedRef.current = true;
+            } catch (error) {
+                console.error("Erreur lors de la récupération des véhicules ou de l'initialisation :", error);
+            }
         };
 
-        document.body.appendChild(script);
-    }, []);
+        fetchAndInit();
+    }, [client]);
 
     return (
         <div
