b46f2f0f062cdeb84aeca772c56107704f9a1be8
Antoine B <aberthillot@myges.fr>
2025-05-22 11:16:26 +0200

    Botpress: sending user data

--- frontend/src/components/BotpressWebchat.jsx
+++ frontend/src/components/BotpressWebchat.jsx
@@ -24,13 +24,25 @@ const BotpressChat = () => {
         } catch (error) {
             console.error("Erreur lors de la récupération des véhicules :", error);
         }
+
     };
 
     useEffect(() => {
-        if (client) {
-            fetchVehicles();
+        if (!client) {
+            return;
         }
+        console.log(client);
+        fetchVehicles();
         console.log(clientVehicles);
+        let clientVehiclesSimplified = clientVehicles.map(vehicleObject => {
+            return {
+                brand: vehicleObject.brand.name,
+                model: vehicleObject.model.name,
+                circulationDate: vehicleObject.circulationDate,
+                mileageKm: vehicleObject.kms,
+                vin: vehicleObject.vin,
+            }
+        })
         const script = document.createElement("script");
         script.src = "https://cdn.botpress.cloud/webchat/v2.5/inject.js";
         script.async = true;
@@ -58,19 +70,25 @@ const BotpressChat = () => {
                 "clientId": "d7b487e3-8fff-4fda-9a7e-17a2b6e48bc1",
                 "user": {
                     "data": {
-                        //"firstName": client.firstName,
-                        "vehicles" : [
-                            {
-
-                            }
-                        ]
+                        "firstName": client.firstName,
+                        "lastName": client.lastName,
+                        "gender": client.gender,
+                        "email": client.email,
+                        "phone": client.phone,
+                        "address": client.address,
+                        "city": client.city,
+                        "zipcode": client.zipcode,
+                        "locationLatitude": client.latitude,
+                        "locationLongitude": client.longitude,
+                        "societyName": client.societyName,
+                        "vehicles" : clientVehiclesSimplified
                     }
                 }
             });
 
-            window.botpress.on("webchat:ready", () => {
-                window.botpress.open();
-            });
+            // window.botpress.on("webchat:ready", () => {
+            //     window.botpress.open();
+            // });
         };
 
         document.body.appendChild(script);
