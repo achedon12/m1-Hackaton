e81bf50695a7fa5f3ef76342357c736d786231ed
Achedon12 <leoderoinleo@gmail.com>
2025-05-23 10:43:14 +0200

    fix: location controller

--- backend/src/Controller/LocationController.php
+++ backend/src/Controller/LocationController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -11,13 +12,17 @@ final class LocationController extends AbstractController
 {
 
     #[Route('/coordinates/{zipcode}', name: 'coordinates', methods: ['GET'])]
-    public function getCoordinatesFromZipcode(string $zipcode): Response
+    public function getCoordinatesFromZipcode(Request $request, string $zipcode): Response
     {
+        $data = json_decode($request->getContent(), true) ?? $request->request->all();
+
         if (!preg_match('/^\d{5}$/', $zipcode)) {
             return $this->json(['error' => 'Invalid zipcode format.'], Response::HTTP_BAD_REQUEST);
         }
 
-        $coordinates = ClientController::getCoordinatesFromZipcode($zipcode, '');
+        $city = $data['city'] ?? '';
+
+        $coordinates = ClientController::getCoordinatesFromZipcode($zipcode, $city);
 
         if ($coordinates === null) {
             return $this->json(['error' => 'Coordinates not found.'], Response::HTTP_NOT_FOUND);
