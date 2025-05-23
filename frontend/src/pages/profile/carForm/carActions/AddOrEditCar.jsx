3f3bd972a7a5ce7a173285f4fdccd6553364d2fa
IPandragonI <mathysf84pro@gmail.com>
2025-05-23 11:47:43 +0200

    REFACTOR FOIRE

--- frontend/src/pages/profile/carForm/carActions/AddOrEditCar.jsx
+++ frontend/src/pages/profile/carForm/carActions/AddOrEditCar.jsx
@@ -1,30 +1,7 @@
-import { useState, forwardRef, useEffect } from "react";
+import {useState, forwardRef, useEffect} from "react";
 import config from "../../../../providers/apiConfig.js";
-import {FormField} from "../../../../components";
 
-const validateField = (name, value) => {
-    let error = "";
-
-    switch (name) {
-        case "registrationNumber":
-            const registrationNumberRegex = /^[A-Z]{2}-\d{3}-[A-Z]{2}$/;
-            if (!registrationNumberRegex.test(value)) error = "La plaque d'immatriculation est invalide.";
-            break;
-        case "kms":
-            if (value < 0) error = "Le kilométrage ne peut pas être négatif.";
-            break;
-        case "circulationDate":
-            const currentYear = new Date().getFullYear();
-            if (value < 1886 || value > currentYear) error = "L'année de circulation est invalide.";
-            break;
-        default:
-            break;
-    }
-
-    return error;
-};
-
-const AddOrEditCar = forwardRef(({ car, onSave }, ref) => {
+const AddOrEditCar = forwardRef(({car, onSave}, ref) => {
     const [brandsAndModelsList, setBrandsAndModelsList] = useState([]);
     const [isFormValid, setIsFormValid] = useState(false);
     const [errors, setErrors] = useState({});
@@ -43,15 +20,17 @@ const AddOrEditCar = forwardRef(({ car, onSave }, ref) => {
         const fetchBrandsAndModels = async () => {
             try {
                 const response = await fetch(`${config.apiBaseUrl}/model/list`, {
-                    method: "GET",
-                    headers: { "Content-Type": "application/json" },
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    }
                 });
                 const data = await response.json();
                 setBrandsAndModelsList(data);
             } catch (error) {
-                console.error("Error fetching brands:", error);
+                console.error('Error fetching brands:', error);
             }
-        };
+        }
 
         if (car) {
             setVehiculeForm({
@@ -76,101 +55,192 @@ const AddOrEditCar = forwardRef(({ car, onSave }, ref) => {
         fetchBrandsAndModels();
     }, [car]);
 
+    const validateField = (name, value) => {
+        let error = "";
+
+        switch (name) {
+            case "registrationNumber":
+                const registrationNumberRegex = /^[A-Z]{2}-\d{3}-[A-Z]{2}$/;
+                if (!registrationNumberRegex.test(value)) error = "La plaque d'immatriculation est invalide.";
+                break;
+            case "kms":
+                if (value < 0) error = "Le kilométrage ne peut pas être négatif.";
+                break;
+            case "circulationDate":
+                const currentYear = new Date().getFullYear();
+                if (value < 1886 || value > currentYear) error = "L'année de circulation est invalide.";
+                break;
+            default:
+                break;
+        }
+
+        return error;
+    }
+
     const handleChange = (e) => {
-        const { name, value } = e.target;
+        const {name, value} = e.target;
+        setVehiculeForm({...vehiculeForm, [name]: value});
+
         const error = validateField(name, value);
+        setErrors({...errors, [name]: error});
 
-        setVehiculeForm({ ...vehiculeForm, [name]: value });
-        setErrors({ ...errors, [name]: error });
-
-        const hasErrors = Object.values({ ...errors, [name]: error }).some((err) => err);
+        const hasErrors = Object.values({...errors, [name]: error}).some((err) => err);
         setIsFormValid(!hasErrors);
-    };
+    }
 
-    const handleSave = () => {
+    const handleSave = async () => {
         if (onSave && isFormValid) {
             onSave(vehiculeForm);
         }
         if (ref.current) {
             ref.current.close();
         }
-    };
+    }
 
     return (
         <dialog ref={ref} id="add_car_modal" className="modal">
             <div className="modal-box">
-                <h3 className="text-lg font-bold">{car ? "Modifier le véhicule" : "Ajouter un véhicule"}</h3>
-
-                <FormField
-                    label="Marque"
-                    name="brand"
-                    type={isCustomBrand ? "text" : "select"}
-                    value={vehiculeForm.brand}
-                    onChange={handleChange}
-                    error={errors.brand}
-                    placeholder="Sélectionnez ou entrez une marque"
-                />
-                {!isCustomBrand ? (
-                    <p className="link text-xs mt-2 cursor-pointer" onClick={() => setIsCustomBrand(true)}>
-                        Ma marque n'est pas dans la liste
-                    </p>
+                {car ? (
+                    <>
+                        <h3 className="text-lg font-bold">Modifier le véhicule</h3>
+                        <fieldset className="fieldset bg-slate-100 border-base-300 rounded-box w-full border p-4 mt-4">
+                            <label className="label w-1/3">Kilométrage</label>
+                            <input
+                                type="number"
+                                name="kms"
+                                className="input input-bordered w-full"
+                                placeholder="Ex: 12000"
+                                value={vehiculeForm.kms}
+                                onChange={handleChange}
+                            />
+                            {errors.kms && <p className="text-red-500">{errors.kms}</p>}
+                        </fieldset>
+                    </>
                 ) : (
-                    <p className="link text-xs mt-2 cursor-pointer" onClick={() => setIsCustomBrand(false)}>
-                        Sélectionner une marque
-                    </p>
+                    <>
+                        <h3 className="text-lg font-bold">Ajouter un véhicule</h3>
+                        <fieldset className="fieldset bg-slate-100 border-base-300 rounded-box w-full border p-4 mt-4">
+                            <label className="label w-1/3">Marque</label>
+                            {isCustomBrand ? (
+                                <input
+                                    type="text"
+                                    name="brand"
+                                    className="input input-bordered w-full"
+                                    placeholder="Entrez une marque"
+                                    value={vehiculeForm.brand}
+                                    onChange={handleChange}
+                                />
+                            ) : (
+                                <select
+                                    className="select select-bordered w-full"
+                                    name="brand"
+                                    value={vehiculeForm.brand}
+                                    onChange={handleChange}
+                                >
+                                    <option value="">Sélectionnez une marque</option>
+                                    {[...new Map(brandsAndModelsList.map((car) => [car.brand.id, car.brand])).values()]
+                                        .map((brand) => (
+                                            <option key={brand.id} value={brand.id}>
+                                                {brand.name}
+                                            </option>
+                                        ))}
+                                </select>
+                            )}
+                            {!isCustomBrand ? (
+                                <p className="link text-xs mt-2 cursor-pointer" onClick={() => setIsCustomBrand(true)}>
+                                    Ma marque n'est pas dans la liste
+                                </p>
+                            ) : (
+                                <p className="link text-xs mt-2 cursor-pointer" onClick={() => setIsCustomBrand(false)}>
+                                    Sélectionner une marque
+                                </p>
+                            )}
+                        </fieldset>
+                    </>
                 )}
 
-                {vehiculeForm.brand && (
-                    <FormField
-                        label="Modèle"
-                        name="model"
-                        type={isCustomModel ? "text" : "select"}
-                        value={vehiculeForm.model}
-                        onChange={handleChange}
-                        error={errors.model}
-                        placeholder="Sélectionnez ou entrez un modèle"
-                    />
-                )}
-                {!isCustomModel ? (
-                    <p className="link text-xs mt-2 cursor-pointer" onClick={() => setIsCustomModel(true)}>
-                        Mon modèle n'est pas dans la liste
-                    </p>
-                ) : (
-                    <p className="link text-xs mt-2 cursor-pointer" onClick={() => setIsCustomModel(false)}>
-                        Sélectionner un modèle
-                    </p>
+                {(vehiculeForm.brand && !car) && (
+                    <fieldset className="fieldset bg-slate-100 border-base-300 rounded-box w-full border p-4 mt-4">
+                        <label className="label w-1/3">Modèle</label>
+                        {isCustomModel ? (
+                            <input
+                                type="text"
+                                name="model"
+                                className="input input-bordered w-full"
+                                placeholder="Entrez un modèle"
+                                value={vehiculeForm.model}
+                                onChange={handleChange}
+                            />
+                        ) : (
+                            <select
+                                className="select select-bordered w-full"
+                                name="model"
+                                value={vehiculeForm.model}
+                                onChange={handleChange}
+                            >
+                                <option value="">Sélectionnez un modèle</option>
+                                {brandsAndModelsList
+                                    .filter((model) => model.brand.id === parseInt(vehiculeForm.brand))
+                                    .map((model) => (
+                                        <option key={model.id} value={model.id}>
+                                            {model.name}
+                                        </option>
+                                    ))}
+                            </select>
+                        )}
+                        {!isCustomModel ? (
+                            <p className="link text-xs mt-2 cursor-pointer" onClick={() => setIsCustomModel(true)}>
+                                Mon modèle n'est pas dans la liste
+                            </p>
+                        ) : (
+                            <p className="link text-xs mt-2 cursor-pointer" onClick={() => setIsCustomModel(false)}>
+                                Sélectionner un modèle
+                            </p>
+                        )}
+                    </fieldset>
                 )}
 
-                <FormField
-                    label="Kilométrage"
-                    name="kms"
-                    type="number"
-                    value={vehiculeForm.kms}
-                    onChange={handleChange}
-                    error={errors.kms}
-                    placeholder="Ex: 12000"
-                />
-                <FormField
-                    label="Année"
-                    name="circulationDate"
-                    type="number"
-                    value={vehiculeForm.circulationDate}
-                    onChange={handleChange}
-                    error={errors.circulationDate}
-                    placeholder="Ex: 2023"
-                />
-                <FormField
-                    label="Plaque d'immatriculation"
-                    name="registrationNumber"
-                    value={vehiculeForm.registrationNumber}
-                    onChange={handleChange}
-                    error={errors.registrationNumber}
-                    placeholder="Ex: AB-123-CD"
-                />
+                {(vehiculeForm.model && !car) && (
+                    <fieldset className="fieldset bg-slate-100 border-base-300 rounded-box w-full border p-4 mt-4">
+                        <label className="label w-1/3 mt-4">Kilométrage</label>
+                        <input
+                            type="number"
+                            name="kms"
+                            className="input input-bordered w-full"
+                            placeholder="Ex: 12000"
+                            value={vehiculeForm.kms}
+                            onChange={handleChange}
+                        />
+                        {errors.kms && <p className="text-red-500">{errors.kms}</p>}
 
-                <button className="btn btn-primary w-full mt-4" onClick={handleSave} disabled={!isFormValid}>
+                        <label className="label w-1/3 mt-4">Année</label>
+                        <input
+                            type="number"
+                            name="circulationDate"
+                            className="input input-bordered w-full"
+                            placeholder="Ex: 2023"
+                            value={vehiculeForm.circulationDate}
+                            onChange={handleChange}
+                        />
+                        {errors.circulationDate && <p className="text-red-500">{errors.circulationDate}</p>}
+
+                        <label className="label w-1/3">Plaque d'immatriculation</label>
+                        <input
+                            type="text"
+                            name="registrationNumber"
+                            className="input input-bordered w-full"
+                            placeholder="Ex: AB-123-CD"
+                            value={vehiculeForm.registrationNumber}
+                            onChange={handleChange}
+                        />
+                        {errors.registrationNumber && <p className="text-red-500">{errors.registrationNumber}</p>}
+                    </fieldset>
+                )}
+
+                <button className={`btn btn-primary w-full mt-4`} onClick={handleSave} disabled={!isFormValid}>
                     {car ? "Modifier le véhicule" : "Ajouter le véhicule"}
                 </button>
+
             </div>
             <form method="dialog" className="modal-backdrop">
                 <button>Fermer</button>
